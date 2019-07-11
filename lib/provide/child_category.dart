69cde9f67ee447a6b241e1ac6c3af22bda30d6c7
Nianyi.Yang <nianyi.yang@datayes.com>
2019-07-11 16:09:02 +0800

    商品上拉加载更多

--- lib/provide/child_category.dart
+++ lib/provide/child_category.dart
@@ -2,13 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shop/model/category.dart';
 
 class ChildCategory extends ChangeNotifier {
-
   // 小类列表
   List<BxMallSubDto> _childCategoryList = [];
 
   List<BxMallSubDto> get childCategoryList => _childCategoryList;
 
   getChildCategory(List<BxMallSubDto> list) {
+//    _categoryId=id;
+    _childIndex = 0;
+    //点击大类时，把子类ID清空
+    _subId = '';
+
+    _page = 1;
+    _noMoreText = '';
+
     // 添加【全部】
     BxMallSubDto all = BxMallSubDto();
     all.mallSubId = '00';
@@ -28,6 +35,34 @@ class ChildCategory extends ChangeNotifier {
   // 改变子类索引
   changeChildIndex(int index) {
     _childIndex = index;
+//    subId = id;
+
+    _page = 1;
+    _noMoreText = '';
+
+    notifyListeners();
+  }
+
+  // 大类 ID TODO 模拟接口用不到
+  String _categoryId = '4';
+
+  // 小类 ID TODO 模拟接口用不到
+  String _subId = '';
+
+  // 列表页数
+  int _page = 1;
+
+  // 显示更多的标识
+  String _noMoreText = '';
+
+  // 每次加载完后+1
+  addPage() {
+    _page++;
+  }
+
+  // 改变noMoreText数据
+  changeNoMore(String text) {
+    _noMoreText = text;
     notifyListeners();
   }
 }
