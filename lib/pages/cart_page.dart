36ce517aa4f9a3e0d7042c5b8d52ce971ac239a9
Nianyi.Yang <nianyi.yang@datayes.com>
2019-07-22 16:01:54 +0800

    购物车 页面布局完成

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter_shop/model/cart_info.dart';
 import 'package:flutter_shop/provide/cart.dart';
 import 'package:provide/provide.dart';
 
+import 'cart/cart_bottom.dart';
+import 'cart/cart_item.dart';
+
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,13 +20,20 @@ class CartPage extends StatelessWidget {
               List<CartInfoModel> cartList =
                   Provide.value<CartProvide>(context).cartList;
 
-              return ListView.builder(
-                itemCount: cartList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(cartList[index].goodsName),
-                  );
-                },
+              return Stack(
+                children: <Widget>[
+                  ListView.builder(
+                    itemCount: cartList.length,
+                    itemBuilder: (context, index) {
+                      return CartItem(cartList[index]);
+                    },
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    child: CartBottom(),
+                  ),
+                ],
               );
             } else {
               return Text('正在加载...');
